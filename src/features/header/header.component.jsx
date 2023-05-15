e11fd2ec076cf2a0e9485618a49b505acbe5f51d
justasti <justas.tirevicius@gmail.com>
2023-05-15 19:27:56 +0300

    add header functionality

--- src/features/header/header.component.jsx
+++ src/features/header/header.component.jsx
@@ -1,27 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { logoutUser } from '../users/users.slice'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const { authUser } = useSelector((state) => state.users)
   return (
     <header>
       <img
         src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png'
         alt='logo img'
       />
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/add'>Add Card</Link>
-        </li>
-      </ul>
+      {authUser && (
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/add'>Add Card</Link>
+          </li>
+        </ul>
+      )}
       <div className='buttons_container'>
-        <Link to='/login'>
-          <button>Log In</button>
-        </Link>
-        <Link to='/signup'>
-          <button>Sign Up</button>
-        </Link>
+        {!authUser ? (
+          <>
+            <Link to='/login'>
+              <button>Log In</button>
+            </Link>
+            <Link to='/signup'>
+              <button>Sign Up</button>
+            </Link>
+          </>
+        ) : (
+          <button onClick={() => dispatch(logoutUser())}>Sign Out</button>
+        )}
       </div>
     </header>
   )
