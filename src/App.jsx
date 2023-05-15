49ab713f35d7936b82f2b8889b443bc71329244f
justasti <justas.tirevicius@gmail.com>
2023-05-15 18:42:26 +0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 function App() {
-  return <div></div>
+  return (
+    <Routes>
+      <Route
+        index
+        element={authUser ? <Homepage /> : <Navigate to='login' />}
+      />
+      <Route
+        path='add'
+        element={authUser ? <Homepage /> : <Navigate to='login' />}
+      />
+      <Route path='login' element={<LoginPage />} />
+      <Route path='signup' element={<SignupPage />} />
+    </Routes>
+  )
 }
 
 export default App
