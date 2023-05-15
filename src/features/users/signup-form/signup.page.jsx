47f2c3aec3e3a3a1151238b2a45ce74ed27d6966
justasti <justas.tirevicius@gmail.com>
2023-05-15 19:36:01 +0300

    add signup form

--- src/features/users/signup-form/signup.page.jsx
+++ src/features/users/signup-form/signup.page.jsx
@@ -1,4 +1,22 @@
 const SignupPage = () => {
-  return <div>SignupPage</div>
+  return (
+    <main>
+      <form id='signup-form'>
+        <div>
+          <label htmlFor='email'>Email Address</label>
+          <input type='text' id='email' />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' />
+        </div>
+        <div>
+          <label htmlFor='confirm'>Confirm password</label>
+          <input type='password' id='confirm' />
+        </div>
+        <input type='submit' value='Create Account' />
+      </form>
+    </main>
+  )
 }
 export default SignupPage
